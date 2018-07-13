149116eb52738f8f242b8bbf147d5e694c43d1f0
ali aziz <aa_connect@outlook.com>
2018-07-13 14:16:12 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,12 +2,12 @@ def unsafe?(speed)
 if speed < 40 || speed > 60
 	return true
 else
-	"safe"
+	false
 end
 end
 
 
 
 def not_safe?(speed)
-speed < 40 || speed > 60 ? true : "unsafe"
+speed < 40 || speed > 60 ? true : false
 end
